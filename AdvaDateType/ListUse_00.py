7a53e220c35c606a1f540e9cb3d9148f05794a56
HuiningBinla <wangbing_0318@163.com>
2019-01-04 00:21:58 +0800

    列表的增加的方法！

--- AdvaDateType/ListUse_00.py
+++ AdvaDateType/ListUse_00.py
@@ -19,10 +19,12 @@ name_list[1] = "李四"
 # 3. 增加
 # append 方法可以在列表的末尾追加数据
 name_list.append("王小二")
-
+# insert 方法可以在列表索引指定的位置插入数据
 name_list.insert(1,"小美眉")
 
-
+# extend 方法可以把其他列表中的完整内容，追加到当前列表的末尾！
+temp_list = ["孙悟空","猪二哥","沙师弟"]
+name_list.extend(temp_list)
 
 # 4. 删除
 
