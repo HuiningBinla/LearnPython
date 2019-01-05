30f20fc03b126a0ae256325090975af101a44b3e
unknown <wangbing_0318@163.com>
2019-01-06 01:26:09 +0800

    字典增加/修改！

--- AdvaDateType/UseDict_00.py
+++ AdvaDateType/UseDict_00.py
@@ -5,10 +5,14 @@ print(xiaoming_dict["name"])
 # 在取值的时候，如果指定的key不存在，程序会报错！
 # print(xiaoming_dict["name123"])
 
-
-# 2. 取值/修改
-
-
-
+# 2. 增加/修改
+# 如果key不存在，会新增键值对
+xiaoming_dict["age"] = 18
+# 如果key存在，会修改已经存在的键值对
+xiaoming_dict["name"] = "小小明"
 
 # 3. 删除
+
+
+print(xiaoming_dict)
+
