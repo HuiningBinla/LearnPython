bc6841c06745b8dbe6348520a3c10ff6c0daaeab
unknown <wangbing_0318@163.com>
2019-01-06 14:18:29 +0800

    字典的循环遍历！

---
+++ AdvaDateType/SearchLoop_00.py
@@ -0,0 +1,13 @@
+xiaoming_dict = {"name":"小明","age":"123456","phone":"10088"}
+
+# 迭代遍历
+# 变量k是每一次循环中，获取到的键值对的key
+for k in xiaoming_dict:
+	
+	print("%s - %s" % (k,xiaoming_dict[k]))
+
+
+
+
+
+# print(xiaoming_dict)
